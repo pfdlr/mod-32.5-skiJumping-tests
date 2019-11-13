b2eadb8a92a71777573ababbc724e8b503b89d82
Piotr Fidler <fidler.piotr@gmail.com>
2019-11-13 08:28:47 +0100

    fix condition

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -3,16 +3,15 @@ const calculateDistancePoints = (distance, hillSize, kPoint) => {
     const bigHillPoints = 60 + (1.8 * (distance - kPoint));
     const mammouthHillPoints = 120 + (1.2 * (distance - kPoint));
 
-    if (typeof hillSize === 'string' && (hillSize === 'normal' || hillSize === 'big' || hillSize === 'mammouth')) {
-        if (hillSize === 'normal') {
-            return normalHillPoints;
-        }
-        else if (hillSize === 'big') {
-            return bigHillPoints;
-        }
-        else if (hillSize === 'mammouth') {
-            return mammouthHillPoints;
-        }
+
+    if (hillSize === 'normal') {
+        return normalHillPoints;
+    }
+    else if (hillSize === 'big') {
+        return bigHillPoints;
+    }
+    else if (hillSize === 'mammouth') {
+        return mammouthHillPoints;
     }
     else {
         console.log('\"hillSize\" must be one of the string: normal, big, mammouth');
